75adb5e0c902b15b4dd1997ddc3aadd09224bc2a
Mark Slorach <hello@markslorach.com>
2024-03-02 17:33:29 +0000

    fixed layout children

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,12 +11,14 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: {
+  children: React.ReactNode
+}) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>
+        <main>{children}</main>
+      </body>
     </html>
-  );
+  )
 }
