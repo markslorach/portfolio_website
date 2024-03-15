07cc877155ac8f39a685816435ee5645c7bce077
Mark Slorach <hello@markslorach.com>
2024-03-15 09:20:49 +0000

    Add Tailwind CSS utilities and update theme configurations

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,12 +1,38 @@
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 // Components
 import ThemeButton from "./components/ThemeButton";
 
+interface NavLinks {
+  label: string;
+  href: string;
+}
+
+const navLinks: NavLinks[] = [
+  { label: "about", href: "/about" },
+  { label: "projects", href: "/projects" },
+  { label: "blog", href: "/blog" },
+];
+
 const NavBar = () => {
+  //   const pathname = usePathname();
+
   return (
-    <nav className="container flex justify-between items-center m-auto max-w-6xl">
-      <Link href="/">Home</Link>
+    <nav className="m-auto flex h-20 max-w-3xl items-center justify-between border-b">
+      <div className="flex items-center space-x-12">
+        <Link href="/">
+          <h1 className="text-2xl font-bold">markslorach.</h1>
+        </Link>
+
+        <ul className="flex space-x-4">
+          {navLinks.map((link, idx) => (
+            <li key={idx}>
+              <Link href={link.href}>{link.label}</Link>
+            </li>
+          ))}
+        </ul>
+      </div>
       <ThemeButton />
     </nav>
   );
--- app/components/ThemeButton.tsx
+++ app/components/ThemeButton.tsx
@@ -9,9 +9,7 @@ const ThemeButton = () => {
   const { resolvedTheme, setTheme } = useTheme();
   const [mounted, setMounted] = useState(false);
 
-  useEffect(() => {
-    setMounted(true);
-  }, []);
+  useEffect(() => setMounted(true), []);
 
   const toggleTheme = () => {
     setTheme(resolvedTheme === "dark" ? "light" : "dark");
@@ -20,18 +18,21 @@ const ThemeButton = () => {
   if (!mounted) return null;
 
   return (
+    <>
+    
     <button
       aria-label="Toggle Theme"
       type="button"
-      className="p-2 rounded-lg bg-slate-200"
+      className="rounded-lg bg-slate-200 p-2"
       onClick={toggleTheme}
     >
       {resolvedTheme === "dark" ? (
-        <SunIcon className="h-5 w-5 text-black/80" />
+        <SunIcon className="h-4 w-4 text-black/80" />
       ) : (
-        <MoonIcon className="h-5 w-5 text-black/80" />
+        <MoonIcon className="h-4 w-4 text-black/80" />
       )}
     </button>
+    </>
   );
 };
 
