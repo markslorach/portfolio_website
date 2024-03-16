ade4f29d955d1f8904e937fa8dd3fc49e3d8e2c1
Mark Slorach <hello@markslorach.com>
2024-03-16 00:58:13 +0000

    fixed build errors

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main>
       <h1 className="text-4xl font-bold text-black/80 dark:text-white/70">
-        Hello, I'm{" "}
+        Hello, I&apos;m{" "}
         <span className="underline decoration-blue-400 decoration-[6px] underline-offset-4">
           Mark
         </span>
