e2c20b6b26cda549f40e92a20922a94f618d5d1d
Mark Slorach <hello@markslorach.com>
2024-03-18 15:54:13 +0000

    update

--- app/(site)/contact/page.tsx
+++ app/(site)/contact/page.tsx
@@ -1,5 +1,5 @@
 const ContactPage = () => {
-  return <h1 className="heading-h2 mb-8">Contact</h1>;
+  return <h1 className="heading-h2 mb-8"><span className="underline decoration-blue-400 decoration-[4px] underline-offset-4">Contact</span>.</h1>
 };
 
 export default ContactPage;
