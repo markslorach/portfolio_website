a8a624b703f6035770af0222cbfb57f31c930fae
Mark Slorach <hello@markslorach.com>
2024-03-21 12:37:00 +0000

    fixed form mobile responsivness

--- app/(site)/contact/Contact.tsx
+++ app/(site)/contact/Contact.tsx
@@ -5,8 +5,8 @@ import { Textarea } from "@/components/ui/textarea";
 
 const Contact = () => {
   return (
-    <form>
-      <div className="grid w-full grid-cols-2 items-center space-x-4">
+    <form className="border rounded-lg p-3">
+      <div className="grid w-full grid-cols-1 sm:grid-cols-2 items-center gap-4">
         <div className="flex flex-col space-y-2">
           <Label htmlFor="name" className="font-semibold">
             Name
@@ -26,7 +26,7 @@ const Contact = () => {
         </Label>
         <Textarea placeholder="Type your message here." id="message" />
       </div>
-      <Button>Send</Button>
+      <Button className="w-full">Get in touch</Button>
     </form>
   );
 };
