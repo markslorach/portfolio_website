09510a19b078231c55732298b72bc0c0b7c60e5d
Mark Slorach <hello@markslorach.com>
2024-04-08 18:19:10 +0100

    bio styling

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -36,7 +36,7 @@ const Hero = () => {
           .
         </Heading>
 
-        <p className="text-3xl font-bold w-full tracking-[0.015rem] text-gray-500 dark:text-gray-400">
+        <p className="text-3xl font-bold sm:text-balance w-full tracking-[0.015rem] text-gray-500 dark:text-gray-400">
         A full-stack JavaScript focused developer based in Scotland, specialising in creating fast, responsive, and user-friendly applications.
         </p>
 
