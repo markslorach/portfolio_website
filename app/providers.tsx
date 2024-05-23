2b49fff42d6fc7fd6eabd5e4c2d13c3f19ce823c
Mark Slorach <hello@markslorach.com>
2024-05-23 12:35:00 +0100

    default dark mode

--- app/providers.tsx
+++ app/providers.tsx
@@ -4,7 +4,7 @@ import { ReactNode } from "react";
 
 const Providers = ({ children }: { children: ReactNode }) => {
   return (
-    <ThemeProvider attribute="class" enableSystem={false} defaultTheme="light">
+    <ThemeProvider attribute="class" enableSystem={false} defaultTheme="dark">
       {children}
     </ThemeProvider>
   );
